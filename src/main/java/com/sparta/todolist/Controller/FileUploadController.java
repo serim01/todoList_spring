d50a40c7e1c6f65d517f1b587a04f2370504a844
serim01 <kkhksl13@gmail.com>
2024-05-20 18:03:16 +0900

    🎉 add:8단계 파일 업로드 & 다운로드
    ### 조건
    
    1. 사용자는 파일을 업로드할 수 있습니다. 업로드된 파일은 `개인 PC에`저장합니다.
        1. 특정 파일 형식만을 업로드할 수 있도록 구현할 수 있습니다.
        2. `JPG`, `PNG`, `JPEG` 등 이미지

---
+++ src/main/java/com/sparta/todolist/Controller/FileUploadController.java
@@ -0,0 +1,37 @@
+package com.sparta.todolist.Controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+@RestController
+public class FileUploadController {
+    private static final String UPLOAD_DIR = "C:/uploads/";
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file) {
+        String fileName = file.getOriginalFilename();
+        if (fileName == null || !isImageFile(fileName)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("이미지 파일만 업로드 가능합니다.");
+        }
+
+        try {
+            File dest = new File(UPLOAD_DIR + file.getOriginalFilename());
+            file.transferTo(dest);
+            return ResponseEntity.ok("파일 업로드 성공: " + file.getOriginalFilename());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("파일 업로드 실패: " + file.getOriginalFilename());
+        }
+    }
+
+    private boolean isImageFile(String fileName) {
+        String lowerCaseFileName = fileName.toLowerCase();
+        return lowerCaseFileName.endsWith(".jpg") || lowerCaseFileName.endsWith(".jpeg") || lowerCaseFileName.endsWith(".png");
+    }
+}
