aa2a9f4b0523794e98fe7649c9b138ee01cf8c24
serim01 <kkhksl13@gmail.com>
2024-05-29 20:26:19 +0900

    🎉 add:5단계 JWT
    ### 기능
    
    - JWT를 이용한 인증/인가를 구현한다.
    - 위 1~4 단계에서 인증/인가가 완료된 후에만 기능이 동작하도록 수정한다.
    
    ### 조건
    
    - Access Token 만료시간 60분
    
    -> jwt 설정에 관련된 부분 + user entity만 추가하였음.

---
+++ src/main/java/com/sparta/todolist/entity/User.java
@@ -0,0 +1,41 @@
+package com.sparta.todolist.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "user")
+@NoArgsConstructor
+public class User extends Timestamped{
+    /*
+    아이디 : bigint
+    별명 : varchar
+    사용자이름 (username) : varchar
+    비밀번호 (password) : varchar
+    권한 (일반, 어드민) : varchar
+    생성일 : timestamp
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String nickname;
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role;
+
+    public User(String username, String password, String nickname, UserRoleEnum role) {
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+        this.role = role;
+    }
+}
