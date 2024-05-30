00f878a5c79e058b76001fd1a121cd861b7cca72
serim01 <kkhksl13@gmail.com>
2024-05-30 20:46:33 +0900

    🩹 fix:Swagger문서 api 설명 변경

--- src/main/java/com/sparta/todolist/config/SwaggerConfig.java
+++ src/main/java/com/sparta/todolist/config/SwaggerConfig.java
@@ -28,7 +28,7 @@ public class SwaggerConfig {
     private Info apiInfo() {
         return new Info()
                 .title("일정 관리 앱 서버") // API의 제목
-                .description("일정 작성, 조회, 수정, 삭제 기능 가능") // API에 대한 설명
+                .description("회원별 일정 기능과 그 일정에 따른 댓글 기능 가능") // API에 대한 설명
                 .version("1.0.0"); // API의 버전
     }
 }
